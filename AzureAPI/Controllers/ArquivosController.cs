0f8a5cbc013efbda79075b253781702c549467c0
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2023-12-03 11:31:54 -0300

    Adicionando base da API e POST.
    Esse commit foi feito dentro da pasta AzureAPI para solucionar um possível bug ao manter todos os arquivos no diretório. Método faz o POST normalmente em containers do Azure.

---
+++ AzureAPI/Controllers/ArquivosController.cs
@@ -0,0 +1,35 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobStorageAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArquivosController : ControllerBase
+    {
+        private readonly string _connectionString;
+        private readonly string _containerName;
+
+        public ArquivosController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("BlobConnectionString");
+            _containerName = configuration.GetValue<string>("BlobContainerName"); 
+        }
+
+        [HttpPost("Upload")]
+        public IActionResult UploadArquivo(IFormFile arquivo)
+        {
+            BlobContainerClient container = new(_connectionString, _containerName);
+            BlobClient blob = container.GetBlobClient(arquivo.FileName);
+
+            using var data = arquivo.OpenReadStream();
+            blob.Upload(data, new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = arquivo.ContentType}
+            });
+
+            return Ok(blob.Uri.ToString());
+        }
+    }
+}
